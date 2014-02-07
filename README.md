cd9bb79f212d0fcfdfe0761256404f7887d6f472
Sobak <msobaczewski@gmail.com>
2014-02-07 17:03:10 +0100

    Init files added

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 wordpress-cleaner
 =================
+
+Z [opisem skryptu](http://sobak.pl/portfolio/wordpress-cleaner/) możesz zapoznać się na moim blogu.
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+if (file_exists('wp-config.php')) {
+	require_once('wp-config.php');
+
+	$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	$mysqli -> query('DELETE FROM '.$table_prefix."posts WHERE post_status = 'inherit'") or die ('Nie udalo sie wykonac zapytania do DB!');
+	echo 'Pomyslnie skasowano '.$mysqli -> affected_rows.' rewizji';
+}
+else {
+	echo 'Nie znaleziono pliku konfiguracyjnego WordPressa';
+}
