23f6f6dbfbf79831e5bdb7a8f50745694b9eac75
Sobak <msobaczewski@gmail.com>
2014-02-09 06:46:10 +0100

    Safer WHERE clause

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ if (file_exists('wp-config.php')) {
 	require_once('wp-config.php');
 
 	$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-	$mysqli -> query('DELETE FROM '.$table_prefix."posts WHERE post_status = 'inherit'") or die ('Nie udalo sie wykonac zapytania do DB!');
+	$mysqli -> query('DELETE FROM '.$table_prefix."posts WHERE post_status = 'inherit' AND post_type = ‘attachment’") or die ('Nie udalo sie wykonac zapytania do DB!');
 	echo 'Pomyslnie skasowano '.$mysqli -> affected_rows.' rewizji';
 }
 else {
